b483aeb4c0b00191e0025378de77dcca2be47831
Xuemin Guan <gxuemin@gmail.com>
2017-02-25 22:13:04 +0000

    Adding test.

--- src/main/scala/com/gildedrose/Item.scala
+++ src/main/scala/com/gildedrose/Item.scala
@@ -1,5 +1,3 @@
 package com.gildedrose
 
-class Item(val name: String, var sellIn: Int, var quality: Int) {
-
-}
+case class Item(val name: String, var sellIn: Int, var quality: Int)
--- src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -3,10 +3,19 @@ package com.gildedrose
 import org.scalatest._
 
 class GildedRoseTest  extends FlatSpec with Matchers {
-      it should "foo" in {
+      it should "Sellin decrease by one but quality does not turn negative." in {
         var items = Array[Item](new Item("foo", 0, 0))
         val app = new GildedRose(items)
         app.updateQuality()
-        (app.items(0).name) should equal ("fixme")
+        app.items.length should be(1)
+        (app.items(0)) should equal (Item("foo", -1, 0))
+      }
+
+      it should "Sellin decrease by one and quality descrease by one" in {
+        var items = Array[Item](new Item("foo", 0, 1))
+        val app = new GildedRose(items)
+        app.updateQuality()
+        app.items.length should be(1)
+        (app.items(0)) should equal (Item("foo", -1, 0))
       }
 }
